9a2c8c06e4e38d9239df43fc621fe9e3590171ac
mriduladas <79037287+mriduladas@users.noreply.github.com>
2021-06-14 14:49:07 +0530

    Update install_docker_hello_java5000.sh

--- install_docker_hello_java5000.sh
+++ install_docker_hello_java5000.sh
@@ -10,6 +10,6 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce
 
-sudo docker pull kaustavdas1987/hello-world-java:0.0.2.RELEASE
+sudo docker pull in28min/hello-world-java
 
-sudo docker run --name sampleapp -p 5000:80 -d kaustavdas1987/hello-world-java:0.0.2.RELEASE
+sudo docker run --name sampleapp -p 80:5000 -d in28min/hello-world-java
